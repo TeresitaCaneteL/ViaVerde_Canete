813012e178c107edbab44ae1cfd86711740a5c01
teresita <t.canette@gmail.com>
2022-07-02 14:20:32 -0400

    corrigiendo organizacion de componentes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,27 +3,13 @@ import './ItemList.css';
 import ItemCount from './ItemCount.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container} from 'react-bootstrap';
-import { useEffect, useState } from 'react';
 
-function ItemList() {
-  const [info, setInfo]=  useState([])
-   useEffect( ()=>{
-    setTimeout(()=>{
-      fetch('data.json',{
-        headers:{
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-  })
-      .then((resp) => resp.json())
-      .then((data) => {setInfo(data)})
-    },3000)
 
-  },[])
+function ItemList({info}) {
   return (
     <Container fluid="md">
        <div className="row">
-         {info && info.map((item) =>{
+         {info?.map((item) =>{
           return (
               <Item key={ item.id } item={ item } />
           )
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -3,13 +3,28 @@ import './ItemListContainer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container} from 'react-bootstrap';
 import ItemList from './ItemList';
-
+import { useEffect, useState } from 'react';
 
 
 function ItemListContainer() {
+  const [info, setInfo]=  useState([])
+  useEffect( ()=>{
+   setTimeout(()=>{
+     fetch('data.json',{
+       headers:{
+         "Content-Type": "application/json",
+         Accept: "application/json",
+       },
+ })
+     .then((resp) => resp.json())
+     .then((data) => {setInfo(data)})
+   },3000)
+
+ },[])
+
   return (
     <Container>
-         <ItemList/>
+         <ItemList info={info}/>
     </Container>
 
   );
