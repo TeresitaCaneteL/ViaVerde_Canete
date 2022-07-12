aa75a9eebd5d9d2b74ead92a93f3f4f64e6f7194
teresita <t.canette@gmail.com>
2022-07-11 23:40:05 -0400

    function cart

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Navbar from './components/nav/Navbar';
 import ItemDetailContainer from './components/itemDetailContainer/ItemDetailContainer'
 import ItemListContainer from './components/itemListContainer/ItemListContainer'
 import NotFound from './components/nav/NotFound'
+import { CartProvider } from './components/context/Context'
 import { Routes, Route} from 'react-router-dom';
 
 
 
 function App() {
   return (
+    <CartProvider>
     <div className="App">
      <Navbar/>
-
      <Routes>
      {/*<Route path="/ViaVerde_Canete" element={< ItemListContainer/>}></Route>*/}
      <Route path="/" element={< ItemListContainer/>}>Home</Route>
@@ -19,9 +20,8 @@ function App() {
      <Route path="/product/:itemId" element={ <ItemDetailContainer />} />
      <Route path="*" element={<NotFound/>}></Route>
      </Routes>
-
-
     </div>
+    </CartProvider>
   );
 }
 
---
+++ src/components/context/Context.js
@@ -0,0 +1,27 @@
+import React, {useState, createContext } from 'react';
+
+
+export const CartContext = createContext({})
+
+const {Provider}=CartContext
+
+export const CartProvider = ({defaultValue = [], children}) =>{
+  const [cart, setCart]=useState(defaultValue);
+  const clearCart=()=>{
+    setCart([]);
+  }
+  const addCart = (newItem, quantity) => {
+    !cart.find(item => item.id === newItem.id) && setCart([...cart, { ...newItem, quantity }]);
+};
+
+  const context = {
+    clearCart,
+    addCart
+  }
+
+  return(
+    <Provider value={context}>
+      {children}
+    </Provider>
+  )
+}
